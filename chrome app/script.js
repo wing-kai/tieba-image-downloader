fb0527c497bd5963cb72f1e75ed9022eb306059e
wing-kai <cyj9385@outlook.com>
2018-01-10 11:42:08 +0800

    更换为vue框架

--- chrome app/script.js
+++ chrome app/script.js
@@ -1,34 +1,10 @@
-const postUrl = document.getElementById('post-url');
-const btnAnalys = document.getElementById('btn-analys');
-const textResult = document.getElementById('text-result');
-const imgWrap = document.getElementById('img-wrap');
-const btnDownload = document.getElementById('btn-download');
+"use strict";
 
-const Store = {
-  imgList: []
-}
-
-// 重命名
-const rename = function(num) {
+const rename = num => {
   return num > 99 ? num : num > 9 ? '0' + num : '00' + num;
 }
 
-// 识别图片html
-const analys = function(htmlDOM) {
-  Store.imgList = Array.prototype.slice.call(
-    htmlDOM.querySelectorAll('img.BDE_Image')
-  ).filter(function(dom) {
-    return /^https\:\/\/imgsa\.baidu\.com\/forum\/.+$/.test(dom.src);
-  }).map(function(dom, i) {
-    return dom.src.replace(/^.+\/sign.+\/(.+)+$/, '$1'); // img ID
-  });
-
-  textResult.innerHTML = '结果：' + Store.imgList.length + '张图片';
-  textResult.removeAttribute('style');
-  btnDownload.removeAttribute('style');
-}
-
-const request = function(url) {
+const request = url => {
   return new Promise(resolve => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -45,41 +21,66 @@ const request = function(url) {
   });
 }
 
-const getImageOnloadHandler = function(fileName) {
-  return function(e) {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
+new Vue({
+  el: '#main',
+  data: {
+    imgList: [],
+    imgElementList: [],
+    url: 'https://tieba.baidu.com/p/5027692610?see_lz=1',
+    buttonDisabled: false,
+    anlaysing: false,
+    showDownloadButton: false
+  },
+  methods: {
+    async analysURL() {
+      const postURL = this.url.trim();
+      const htmlElement = await request(postURL);
 
-    canvas.height = this.height;
-    canvas.width = this.width;
-    ctx.drawImage(this, 0, 0);
+      Object.assign(this, {
+        url: '',
+        anlaysing: true,
+        showDownloadButton: false
+      });
 
-    canvas.toBlob(function(blob) {
-      var a = document.createElement('a');
-      a.download = fileName;
-      a.href = window.URL.createObjectURL(blob);
-      a.click();
-      window.URL.revokeObjectURL(a.href);
-    }, 'image/jpeg', 1);
+      this.imgList = Array.prototype.slice.call(
+        htmlElement.querySelectorAll('img.BDE_Image')
+      ).filter(dom => {
+        return /^https\:\/\/imgsa\.baidu\.com\/forum\/.+$/.test(dom.src);
+      }).map((dom, i) => {
+        return dom.src.replace(/^.+\/sign.+\/(.+)+$/, '$1'); // img ID
+      });
+
+      Object.assign(this, {
+        anlaysing: false,
+        showDownloadButton: true
+      });
+    },
+
+    async downloadImage() {
+      let elementList = [];
+      elementList = this.imgList.map((fileName, i) => {
+        return 'http://imgsrc.baidu.com/forum/pic/item/' + fileName;
+      });
+
+      this.imgElementList = elementList;
+    },
+
+    handleImageLoaded(e, i) {
+      const fileName = rename(i);
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+
+      canvas.height = e.currentTarget.height;
+      canvas.width = e.currentTarget.width;
+      ctx.drawImage(e.currentTarget, 0, 0);
+
+      canvas.toBlob(blob => {
+        var a = document.createElement('a');
+        a.download = fileName + '.jpg';
+        a.href = window.URL.createObjectURL(blob);
+        a.click();
+        window.URL.revokeObjectURL(a.href);
+      }, 'image/jpeg', 1);
+    }
   }
-}
-
-btnAnalys.addEventListener('click', async function(e) {
-  const url = postUrl.value.trim();
-  postUrl.value = '';
-  btnAnalys.setAttribute('disabled', 'disable');
-
-  const htmlDOM = await request(url);
-  analys(htmlDOM);
-  return true;
-});
-
-btnDownload.addEventListener('click', function() {
-  Store.imgList.forEach(function(fileName, i) {
-    const img = new Image();
-    img.src = 'http://imgsrc.baidu.com/forum/pic/item/' + fileName;
-    img.setAttribute('style', 'position:absolute;visibility:hidden;');
-    img.onload = getImageOnloadHandler(rename(i));
-    imgWrap.appendChild(img);
-  });
 });
